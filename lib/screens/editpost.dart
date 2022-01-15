0e6e7e51439fd611c6d7b02508a5ddfeaa59f589
devammarzd <ammar200698@gmail.com>
2022-01-15 16:43:21 +0500

    UI Fixes in TextFormFields

--- lib/screens/editpost.dart
+++ lib/screens/editpost.dart
@@ -10,6 +10,12 @@ class EditPostScreen extends StatefulWidget {
 }
 
 class _EditPostScreenState extends State<EditPostScreen> {
+  TextEditingController idController = TextEditingController();
+
+  TextEditingController titleController = TextEditingController();
+
+  TextEditingController bodyController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,95 +26,97 @@ class _EditPostScreenState extends State<EditPostScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(11, 8, 11, 8),
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Container(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: TextFormField(
-                    obscureText: true,
-                    controller: TextEditingController(),
+          child: Form(
+            key: _formKey,
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: TextFormField(
+                      controller: idController,
+                      validator: (val) {
+                        return val!.isEmpty ? "Please Enter an ID" : null;
+                      },
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: "Enter ID",
+                          prefixIcon: Icon(Icons.confirmation_number_outlined),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: TextFormField(
+                      controller: titleController,
+                      validator: (val) {
+                        return val!.isEmpty ? "Please Enter Title" : null;
+                      },
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                          labelText: "Enter Title",
+                          prefixIcon: Icon(Icons.post_add),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ))),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: bodyController,
                     validator: (val) {
-                      return null;
+                      return val!.isEmpty ? "Please Enter Body" : null;
                     },
+                    maxLines: 5,
                     keyboardType: TextInputType.name,
                     decoration: InputDecoration(
-                        labelText: "Enter ID",
-                        prefixIcon: Icon(Icons.confirmation_number_outlined),
+                        labelText: "Enter Body",
+                        alignLabelWithHint: true,
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(15),
                             borderSide: BorderSide(
                               color: Colors.black,
                             ))),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: TextFormField(
-                    obscureText: true,
-                    controller: TextEditingController(),
-                    validator: (val) {
-                      return null;
-                    },
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        labelText: "Enter Title",
-                        prefixIcon: Icon(Icons.post_add),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            ))),
+                  Expanded(
+                    child: Container(),
                   ),
-                ),
-                TextFormField(
-                  controller: TextEditingController(),
-                  validator: (val) {
-                    return null;
-                  },
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                      labelText: "Enter Body",
-                      prefixIcon: Icon(Icons.dehaze_outlined),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15),
-                          borderSide: BorderSide(
-                            color: Colors.black,
-                          ))),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(primary: Colors.green),
-                      onPressed: () {},
-                      child: Text(
-                        "EDIT",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: Colors.green),
+                        onPressed: () {},
+                        child: Text(
+                          "EDIT",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Container(),
-                ),
-              ],
+                  Expanded(
+                    flex: 5,
+                    child: Container(),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
